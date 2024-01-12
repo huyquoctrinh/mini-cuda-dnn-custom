9b1bd3a7b63744d146db4fb1e503dc7ba1883c97
Thao Vo <thao.vo@navercorp.com>
2024-01-12 11:04:13 +0700

    demo

--- demo.cc
+++ demo.cc
@@ -6,12 +6,22 @@ void inference_only(int batch_size) {
   MNIST dataset("./data/mnist/");
   dataset.read();
   std::cout<<"Done"<<std::endl;
-
+  
   std::cout<<"Loading model...";
   Network dnn = createNetwork_GPU();
   std::cout<<"Done"<<std::endl;
 
+  std::cout<<"Start Forward"<<std::endl;
+  auto start = high_resolution_clock::now();
+
   dnn.forward(dataset.test_data);
+
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  std::cout << "Time taken: "
+         << duration.count() / 1000 << " milliseconds" << std::endl;
+  
   float acc = compute_accuracy(dnn.output(), dataset.test_labels);
   std::cout<<std::endl;
   std::cout<<"Test Accuracy: "<<acc<< std::endl;
@@ -21,7 +31,7 @@ void inference_only(int batch_size) {
 int main(int argc, char* argv[]) {
 
   int batch_size = 10000;
-
+  
   if(argc == 2){
     batch_size = atoi(argv[1]);
   }
